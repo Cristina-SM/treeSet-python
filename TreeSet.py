966973674320401855e5b804717354dc415b4402
Usuario <juan.rodriguez215@alu.ulpgc.es>
2024-04-18 11:40:36 +0100

    Contains, iter, next, descendingOrder

--- TreeSet.py
+++ TreeSet.py
@@ -30,12 +30,6 @@ class TreeSet:
                     self.lista.append(value)
                     self.root = self.padreTotal
 
-    # def contains(self, value):
-    #     if self.root == None:
-    #         return False
-    #     else:
-    #         return self.root.contains(value)
-
     def remove(self, value):
         switcher = False
         if self.root is None:
@@ -90,8 +84,40 @@ class TreeSet:
                     return True
                 # aqui falta baaastante codigo todavia, es un simple esquema de como se podria hacer
 
+    def contains(self, value):
+        if self.root == None:
+            return False
+        else:
+            while self.root is not None:
+                if value < self.root.key:
+                    while self.root.left is not None:
+                        self.root = self.root.left
+                        if value == self.root.key:
+                            return True
+                        if self.root.left is None:
+                            return False
+                elif value > self.root.key:
+                    while self.root.right is not None:
+                        self.root = self.root.right
+                        if value == self.root.key:
+                            return True
+                        if self.root.right is None:
+                            return False
+                elif value == self.root.key:
+                    return True
+
     def __str__(self):
         if self.root == None:
             return ""
         else:
             return f"{sorted(self.lista)}"
+        
+    def descendingIterator(self):
+        iterador = iter(self.lista[::-1])
+        for _ in range (0, len(self.lista)):
+            print(next(iterador))
+
+    def __iter__(self):
+        return self.lista.__iter__()
+    def __next__(self):
+        return self.lista.__next__()

f279ea28e7cb32822f81ad994f411e55d9a810ab
Cristina-SM <cristinasantanamartindevelop@gmail.com>
2024-04-29 21:16:21 +0100

    Implementación de AVL + adaptación de los métodos

--- Nodo.py
+++ Nodo.py
@@ -1,24 +1,7 @@
-from functools import total_ordering
-
 class Node:
-    def __init__(self, key, node):
+    def __init__(self, key):
         self.key = key
-        self.parent = node
+        self.parent = None
         self.right = None
         self.left = None
-    def __lt__(self, other):
-        return self.key < other.key
-    def __gt__(self, other):
-        return self.key > other.key
-    def __eq__(self, other):
-        if other == None:
-            return False
-        return self.key == other.key
-    def remove(self, node):
-        node.key == None
-        node.parent = None
-        node.right = None
-        node.left = None
-    def __str__(self):
-        return f"{self.key}"
-    
+        self.height = 1

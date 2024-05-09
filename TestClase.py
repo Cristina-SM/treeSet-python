ab5198fa351c70af99ae65777aaa3b41e6575d1d
DAVIDPC <carapeo@carapeo.com>
2024-05-09 11:33:17 +0100

    3

--- TestClase.py
+++ TestClase.py
@@ -1,16 +1,20 @@
 import unittest
 from TreeSet import TreeSet
-import Coches
+from Coches import Coches
 
 class TestCoche(unittest.TestCase):
     def setUp(self):
         self.tree = TreeSet(Coches)
+        self.coche1 = Coches("4578LMZ", "Toyota")
+        self.coche2 = Coches("1234ABC", "Ford")
+        self.tree.add(self.coche1)
 
     def test_addCoche(self):
-        self.assertTrue(self.tree.add("4578LMZ"))
+        self.assertTrue(self.tree.add(self.coche2))
 
     def test_removeCoche(self):
-        self.assertTrue(self.tree.remove("4578LMZ"))
+        self.assertTrue(self.tree.remove(self.coche1))
+
 
 if __name__ == '__main__':
     unittest.main()

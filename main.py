ec372b9701f378a696a532b83a97cc5a94663eb3
Usuario <juan.rodriguez215@alu.ulpgc.es>
2024-05-09 10:52:07 +0100

    Add y Remove con excepciones, init con tipo

--- main.py
+++ main.py
@@ -4,68 +4,72 @@ import time
 
 def main():
     print("Treeset new")
-    tree_set_new = TreeSet()
-    tree_set_new.add("b")
-    tree_set_new.add(1)
-    tree_set_new.add("d")
-    # tree_set_new.add("g")
-    # tree_set_new.add("m")
-    # tree_set_new.add("H")
-    # tree_set_new.add("a")
-    # tree_set_new.add("C")
-
+    tree_set_new = TreeSet(int)
+    #tree_set_new.add("b")
+    #tree_set_new.add(1);
     print(tree_set_new)
-    print(tree_set_new.size())
-    print(tree_set_new.contains("a"))
-    iterador = tree_set_new.descendingIterator()
-    print(tree_set_new.__repr__)
-    pepe = tree_set_new.clone()
-    print(pepe.__repr__)
-    print("Iterador descendente")
-    for _ in range(0, tree_set_new.size()):
-        print(next(iterador))
-    # new = tree_set_new.clone()
-    tree_set_new.remove("b")
+    #tree_set_new.remove(1)
+    # tree_set_new.add("b")
+    # tree_set_new.add(1)
+    # tree_set_new.add("d")
+    # # tree_set_new.add("g")
+    # # tree_set_new.add("m")
+    # # tree_set_new.add("H")
+    # # tree_set_new.add("a")
+    # # tree_set_new.add("C")
+
+    # print(tree_set_new)
+    # print(tree_set_new.size())
     # print(tree_set_new.contains("a"))
+    # iterador = tree_set_new.descendingIterator()
+    # print(tree_set_new.__repr__)
+    # pepe = tree_set_new.clone()
+    # print(pepe.__repr__)
+    # print("Iterador descendente")
+    # for _ in range(0, tree_set_new.size()):
+    #     print(next(iterador))
+    # # new = tree_set_new.clone()
+    # tree_set_new.remove("b")
+    # # print(tree_set_new.contains("a"))
+    # # print(tree_set_new)
+    # # print(new)
+    # # tree_set_new.clear()
+    # # print(tree_set_new)
+
+    # print("Método ceiling")
+    # print(tree_set_new.ceiling("b"))
+
+    # iter1 = tree_set_new.__iter__()
+    # print("Iterador normal")
+    # for _ in range(0, tree_set_new.size()):
+    #     print(next(iter1))
+
+    # print("Método first")
+    # print(tree_set_new.first())
+
+    # print("Método isEmpty")
+    # print(tree_set_new.isEmpty())
+
+    # print("Método last")
+    # print(tree_set_new.last())
+
+    # print("Método lower")
+    # print(tree_set_new.lower("b"))
+
+    # print("Método floor")
+    # print(tree_set_new.floor("b"))
+
+    # print("Método higher")
+    # print(tree_set_new.higher("b"))
+
+    # print("Método pollLast")
+    # print(tree_set_new.pollLast())
+
+    # print("Método pollFirst")
+    # print(tree_set_new.pollFirst())
+
+    # print("Lista actualizada")
     # print(tree_set_new)
-    # print(new)
-    # tree_set_new.clear()
-    # print(tree_set_new)
-
-    print("Método ceiling")
-    print(tree_set_new.ceiling("b"))
-
-    iter1 = tree_set_new.__iter__()
-    print("Iterador normal")
-    for _ in range(0, tree_set_new.size()):
-        print(next(iter1))
-
-    print("Método first")
-    print(tree_set_new.first())
-
-    print("Método isEmpty")
-    print(tree_set_new.isEmpty())
-
-    print("Método last")
-    print(tree_set_new.last())
-
-    print("Método lower")
-    print(tree_set_new.lower("b"))
-
-    print("Método floor")
-    print(tree_set_new.floor("b"))
-
-    print("Método higher")
-    print(tree_set_new.higher("b"))
-
-    print("Método pollLast")
-    print(tree_set_new.pollLast())
-
-    print("Método pollFirst")
-    print(tree_set_new.pollFirst())
-
-    print("Lista actualizada")
-    print(tree_set_new)
 
 def test_times():
     with open("data/times.csv", mode="w", encoding="utf-8") as fw:
@@ -74,7 +78,7 @@ def test_times():
 
         while size < 17 * 10 ** 6:
             # Registro del tiempo de inicio
-            tree = TreeSet()
+            tree = TreeSet(int)
             start_time = time.perf_counter()
 
             for i in range(size):
@@ -83,12 +87,12 @@ def test_times():
             # Registro del tiempo de finalización
             end_time = time.perf_counter()
             duration = (end_time - start_time) * 1000
-            print("Tiempo de ejecución:", duration, "milisegundos")
+            print(f"Tiempo de ejecución ({size}):", duration, "milisegundos")
 
             print(f"{size};{round(duration, 3)};{round(duration / 1000, 3)}".replace(".", ","), file=fw)
 
             size <<= 1
 
 if __name__ == "__main__":
-    main()
-    #test_times()
+    #main()
+    test_times()

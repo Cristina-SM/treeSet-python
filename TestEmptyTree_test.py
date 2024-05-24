a9afdfcc4dee10bce512c618e787ca82e81aa4a4
Juanquirr <juanquirr579@gmail.com>
2024-05-24 12:16:46 +0100

    Final

--- TestEmptyTree_test.py
+++ TestEmptyTree_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from NullPointerException import NullPointerException
+from NoSuchElementException import NoSuchElementException
 from TreeSet import TreeSet
 
 
@@ -107,6 +108,62 @@ class EmptyTree(unittest.TestCase):
             "The tree should not contain the element 'a' as it has not been added.",
         )
 
+    def testDescendingIterator(self):
+        """
+        Test case for the descendingIterator method.
+
+        This test case verifies that the descendingIterator method returns an empty iterator when the tree is empty.
+
+        Steps:
+        1. Create an empty tree.
+        2. Call the descendingIterator method on the tree.
+        3. Iterate over the iterator and append each element to a list.
+        4. Assert that the list is empty.
+
+        Expected behavior:
+        - The descendingIterator method should return an empty iterator.
+        - The list of elements should be empty.
+
+        """
+        self.iterador = self.tree.descendingIterator()
+        lista = []
+        for _ in range(0, self.tree.size()):
+            lista.append(next(self.iterador))
+        self.assertEqual(lista, [])
+    
+    def testFirstNonExistingElement(self):
+        """
+        Test case to verify that a NoSuchElementException is raised when calling the first() method on an empty tree.
+        """
+        self.assertRaises(NoSuchElementException, self.tree.first)
+    
+    def testFloorNonExistingElement(self):
+        """
+        Test case to verify the behavior of the floor method when the element does not exist in the tree.
+
+        The floor method should return None when the specified element is not present in the tree.
+
+        """
+        self.assertEqual(self.tree.floor("a"), None)
+    
+    def testHigherNonExistingElement(self):
+        """
+        Test case to verify the behavior of the higher method when the element does not exist in the tree.
+
+        The higher method should return None when there is no element in the tree that is strictly greater than the given element.
+
+        """
+        self.assertEqual(self.tree.higher("a"), None)
+    
+    def testIsEmpty(self):
+        """
+        Test case to check if the tree is empty.
+
+        This method asserts that the `isEmpty` method of the `tree` object returns True,
+        indicating that the tree is empty.
+        """
+        self.assertTrue(self.tree.isEmpty())
+
     def testIter(self):
         """
         Tests the iterator of the TreeSet.
@@ -122,13 +179,44 @@ class EmptyTree(unittest.TestCase):
             lista, [], "The iterator should yield an empty list for an empty tree."
         )
 
-    # def testPollFirst(self):
-    # """
-    # Tests polling the first element from the TreeSet.
-    #
-    # This test would verify that the pollFirst method removes and returns the first element
-    # in the TreeSet. (Currently not implemented)
-    # """
+
+    def testLastNonExistingElement(self):
+        """
+        Test case to verify that the `last` method raises a `NoSuchElementException`
+        when called on an empty tree.
+        """
+        self.assertRaises(NoSuchElementException, self.tree.last)
+
+    def testLowerNonExistingElement(self):
+        """
+        Test case to verify the behavior of the lower method when the element does not exist in the tree.
+
+        The lower method should return None when there is no element in the tree that is strictly less than the given element.
+
+        """
+        self.assertEqual(self.tree.lower("a"), None)
+
+    def testPollFirst(self):
+        """
+        Test the pollFirst method of the tree.
+
+        This method checks if the pollFirst method returns None when called on an empty tree.
+
+        Returns:
+            None
+        """
+        self.assertIsNone(self.tree.pollFirst())
+    
+    def testPollLast(self):
+        """
+        Test the `pollLast` method of the tree.
+
+        This method checks if the `pollLast` method returns `None` when called on an empty tree.
+
+        Returns:
+            None
+        """
+        self.assertIsNone(self.tree.pollLast())
 
     def testRemoveNonExistingElement(self):
         """
@@ -160,7 +248,6 @@ class EmptyTree(unittest.TestCase):
             0,
             "The size of the tree should be 0 for a new, empty tree.",
         )
-
 
 if __name__ == "__main__":
     unittest.main()
